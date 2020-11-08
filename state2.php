a0d27435a1bc75f3c81f0ce0b37cc81ae04650c5
Z <kerenyi.nagy.zoltan@gmail.com>
2020-11-08 14:53:21 +0100

    Update state2.php

--- state2.php
+++ state2.php
@@ -3,7 +3,7 @@
         print('State wasnt set');
         exit;
     }else if (filter_input(INPUT_GET, 'state', FILTER_SANITIZE_STRING)){
-        $state = $_GET['state'];
-        print("<p>You live in $state</p>");
+        //$state = $_GET['state'];
+        print('<p>You live in ' . $_GET['state'] . '</p>');
     }        
 ?>
